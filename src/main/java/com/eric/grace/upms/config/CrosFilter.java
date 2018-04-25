7909ccd4d62209ddeb499a1191fb039a8e69bc4b
MrEric <xinyi.zhang@phkjcredit.com>
2018-04-25 14:55:27 +0800

    更新跨域

--- src/main/java/com/eric/grace/upms/config/CrosFilter.java
+++ src/main/java/com/eric/grace/upms/config/CrosFilter.java
@@ -3,6 +3,7 @@ package com.eric.grace.upms.config;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
+import javax.servlet.FilterConfig;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -16,23 +17,28 @@ import java.io.IOException;
 @Component
 public class CrosFilter implements Filter {
 
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+    @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        String method = ((HttpServletRequest) req).getMethod();
         HttpServletResponse response = (HttpServletResponse) res;
-
-        HttpServletRequest reqs = (HttpServletRequest) req;
-
         response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "x-requested-with,Authorization,Content-Type");
+        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Authorization,Content-Type, Accept, Token");
+        if ("OPTIONS".equals(method)) {
+            response.setStatus(HttpServletResponse.SC_OK);
+        }
         chain.doFilter(req, res);
     }
 
-    public void init(FilterConfig filterConfig) {
-    }
-
+    @Override
     public void destroy() {
+
     }
 
 
