2bbc326c1df057230c85c00cd44258c9706df813
MrEric <xinyi.zhang@phkjcredit.com>
2018-04-25 11:49:52 +0800

    修改

---
+++ src/main/java/com/eric/grace/upms/modules/sys/controller/SysDeptController.java
@@ -0,0 +1,23 @@
+package com.eric.grace.upms.modules.sys.controller;
+
+import com.eric.grace.upms.modules.sys.service.ISysDeptService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * SysDeptController: 部门管理
+ *
+ * @author: MrServer
+ * @since: 2018/4/25 上午11:48
+ */
+@RestController
+@RequestMapping("/sys/dept")
+public class SysDeptController extends AbstractController {
+    
+    @Autowired
+    private ISysDeptService deptService;
+
+
+
+}
